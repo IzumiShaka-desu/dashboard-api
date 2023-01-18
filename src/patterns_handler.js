2cab4c1f0f0ebf762141285041332457bb7bae7b
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-18 13:10:23 +0700

    test dulu

--- src/patterns_handler.js
+++ src/patterns_handler.js
@@ -1,8 +1,12 @@
 const mpsPatternHandler = (request, h) => {
-
-
-
+    return h.response({
+        status: 'success',
+        data: {
+            pattern: 'MPS',
+        }
+    },).code(200);
 }
+
 const wpsPatternHandler = (request, h) => {
 
 

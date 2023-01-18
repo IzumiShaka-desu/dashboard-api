fc4fd85baac1c318f0e57d906ee162911bf8a2f9
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-18 09:37:07 +0700

    change ip

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: '10.19.22.143',
     routes: {
       cors: {
         origin: ['*'],

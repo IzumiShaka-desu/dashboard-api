04ba6690a7097f30b3b1f9d66ef3f532d4158e14
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-18 09:56:36 +0700

    create pattern handler

---
+++ src/patterns_handler.js
@@ -0,0 +1,17 @@
+const mpsPatternHandler = (request, h) => {
+
+
+
+}
+const wpsPatternHandler = (request, h) => {
+
+
+
+}
+const woPatternHandler = (request, h) => {
+}
+module.exports = {
+    mpsPatternHandler,
+    wpsPatternHandler,
+    woPatternHandler,
+}

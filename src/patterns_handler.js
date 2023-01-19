f7baa1934eff2f643c5bd62435679a9638fe3dbb
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-19 09:01:49 +0700

    test pembaca pikiran

--- src/patterns_handler.js
+++ src/patterns_handler.js
@@ -1,16 +1,51 @@
 const { getMpsPattern } = require('./pool');
 const mpsPatternHandler = async (request, h) => {
-    let results = await getMpsPattern();
+    let results = (await getMpsPattern()).map((item) => {
+        return {
+            tanggal_mps: item.tanggal_mps,
+            line: item.line_mps,
+            qty: item.qty_mps,
+            type: item.type,
+        }
+    },);
+    let lines = [...new Set(results.map((item) => item.line))];
+    let dates = [...new Set(results.map((item) => item.tanggal_mps))];
+    let types = [...new Set(results.map((item) => item.type))];
+    let result = [];
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        let lineData = results.filter((item) => item.line == line);
+        let lineResult = {
+            line: line,
+            data: [],
+        };
+        for (let j = 0; j < dates.length; j++) {
+            let date = dates[j];
+            let dateData = lineData.filter((item) => item.tanggal_mps == date);
+            let dateResult = {
+                tanggal_mps: date,
+                data: [],
+            };
+            for (let k = 0; k < types.length; k++) {
+                let type = types[k];
+                let typeData = dateData.filter((item) => item.type == type);
+                let typeResult = {
+                    type: type,
+                    qty: 0,
+                };
+                if (typeData.length > 0) {
+                    typeResult.qty = typeData[0].qty;
+                }
+                dateResult.data.push(typeResult);
+            }
+            lineResult.data.push(dateResult);
+        }
+        result.push(lineResult);
+    }
+
     return h.response({
         status: 'success',
-        data: results.map((item) => {
-            return {
-                tanggal_mps: item.tanggal_mps,
-                line: item.line_mps,
-                qty: item.qty_mps,
-                type: item.type,
-            }
-        },),
+        data: result,
     },).code(200);
 }
 

a9241c51eea5cd5bdb06b198c6323977eecd1ccb
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-19 15:06:20 +0700

    add new route

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,24 @@ const { deletebookByIdHandler } = require('./handler');
 const { editbookByIdHandler } = require('./handler');
 const { getbookByIdHandler } = require('./handler');
 const { addbookHandler, getAllbooksHandler } = require('./handler');
-const { mpsPatternHandler, wpsPatternHandler, woPatternHandler } = require('./patterns_handler');
+const { mpsPatternHandler, wpsPatternHandler, woPatternHandler, mpsRawHandler } = require('./patterns_handler');
 
 const routes = [
   {
     method: 'GET',
     path: '/patterns/mps',
     handler: mpsPatternHandler,
+    options: {
+      cache: {
+        expiresIn: (60 * 60) * 1000,
+        privacy: 'private'
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/raw/mps',
+    handler: mpsRawHandler,
     options: {
       cache: {
         expiresIn: (60 * 60) * 1000,

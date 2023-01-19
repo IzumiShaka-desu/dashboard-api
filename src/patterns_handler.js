6dc2ed069d43be57fa0e27ffcd376ef436a216c9
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-19 09:03:43 +0700

    duplicate njir

--- src/patterns_handler.js
+++ src/patterns_handler.js
@@ -9,43 +9,11 @@ const mpsPatternHandler = async (request, h) => {
         }
     },);
     let lines = [...new Set(results.map((item) => item.line))];
-    let dates = [...new Set(results.map((item) => item.tanggal_mps))];
-    let types = [...new Set(results.map((item) => item.type))];
-    let result = [];
-    for (let i = 0; i < lines.length; i++) {
-        let line = lines[i];
-        let lineData = results.filter((item) => item.line == line);
-        let lineResult = {
-            line: line,
-            data: [],
-        };
-        for (let j = 0; j < dates.length; j++) {
-            let date = dates[j];
-            let dateData = lineData.filter((item) => item.tanggal_mps == date);
-            let dateResult = {
-                tanggal_mps: date,
-                data: [],
-            };
-            for (let k = 0; k < types.length; k++) {
-                let type = types[k];
-                let typeData = dateData.filter((item) => item.type == type);
-                let typeResult = {
-                    type: type,
-                    qty: 0,
-                };
-                if (typeData.length > 0) {
-                    typeResult.qty = typeData[0].qty;
-                }
-                dateResult.data.push(typeResult);
-            }
-            lineResult.data.push(dateResult);
-        }
-        result.push(lineResult);
-    }
+
 
     return h.response({
         status: 'success',
-        data: result,
+        data: results,
     },).code(200);
 }
 

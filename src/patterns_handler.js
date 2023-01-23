c91469fb24956471e4bf9939f9ff7301820ee563
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-23 08:40:55 +0700

    add get wps pattern handler

--- src/patterns_handler.js
+++ src/patterns_handler.js
@@ -1,4 +1,4 @@
-const { getMpsPattern } = require('./pool');
+const { getMpsPattern, getWpsPattern } = require('./pool');
 const mpsPatternHandler = async (request, h) => {
     let results = (await getMpsPattern()).map((item) => {
         return {
@@ -75,12 +75,65 @@ const mpsRawHandler = async (request, h) => {
     },).code(200);
 }
 
-const wpsPatternHandler = (request, h) => {
+const wpsPatternHandler = async (request, h) => {
+    let results = (await getWpsPattern()).map((item) => {
+        return {
+            tanggal_mps: item.tanggal_wps,
+            line: item.line_wps,
+            qty: item.qty_wps,
+            type: item.series,
+        }
+    },);
+    let lines = [...new Set(results.map((item) => item.line))].sort();
+    let date = new Date()
+    // let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    // create array of Date(object) from first day of this month to last day of this month
+    // let dateOfThisMonth = Array.from(new Array(lastDay.getDate()), (val, index) => new Date(date.getFullYear(), date.getMonth(), index + 1, 0));
+
+    let result = lines.map((line) => {
+        let data = results.filter((item) => item.line == line);
+        //this using type on this line because to make sure every type ini this line is included
+        // you can also change this scope to type on this date 
+        let typeOnThisLine = [...new Set(data.map((item) => item.type))];
+        let dataByType = typeOnThisLine.map((type) => {
+            let dataByType = data.filter((item) => item.type == type);
+            let dateOnThisType = [...new Set(dataByType.map((item) => item.tanggal_mps))].sort();
+            return {
+                type: type,
+                total: dataByType.reduce((acc, cur) => acc + cur.qty, 0),
+                data: dateOnThisType.map((date) => {
+                    let dataByDate = dataByType.filter((item) => {
+                        // format date to string yyyy-mm-dd to compare with date string
+                        let dateStr = new Date(item.tanggal_mps).toISOString().split('T')[0];
+                        let dateStr2 = new Date(date).toISOString().split('T')[0];
+                        return dateStr === dateStr2;
+                    });
+                    return {
+                        date: date,
+                        total: dataByDate.reduce((acc, cur) => acc + cur.qty, 0),
+                        data: dataByDate,
+                    }
+                }),
+            };
+        });
+
+
+        return {
+            line: line,
+            data: dataByType,
+            title: "MPS Pattern ",
+        }
+    });
+
 
     return h.response({
-        status: 'error',
-        message: 'WPS Pattern is not implemented yet',
-    }).code(500);
+        status: 'success',
+        data: {
+            //MPS Pattern <current month string><current year>
+            title: `MPS Pattern ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`,
+            items: result,
+        },
+    },).code(200);
 
 }
 const woPatternHandler = (request, h) => {
